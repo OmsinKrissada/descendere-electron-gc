761c1c1c009572287c974a770c09c3c7b52c7c95
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-01-29 17:26:43 +0700

    before vuetify

---
+++ src/components/TelemetryBox.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="box">
+		<div class="bar">
+			<div class="left">
+				<h2>TELEMETRY BOX</h2>
+			</div>
+
+			<div class="right">
+				<div class="group">
+					<p class="title">Total Packet Count:</p>
+					<p class="code">{{ total_pkg }}</p>
+				</div>
+				<div class="group">
+					<p class="title">Last Command:</p>
+					<p class="code">{{ last_cmd }}</p>
+				</div>
+			</div>
+		</div>
+		<div id="input"></div>
+	</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class TelemetryBox extends Vue {
+	total_pkg = 0;
+	last_cmd = "-";
+}
+</script>
+
+<style scoped lang="scss"></style>
